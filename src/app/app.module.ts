1bc4fd5027a45a05cbb5f2fd1a3bba8bf965fe90
syqrefind <syqrefind@gmail.com>
2019-03-02 18:08:09 -0500

    adds registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,38 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+import { MatNativeDateModule, MatDatepickerModule, MatIconModule,
+  MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule,
+  MatFormFieldModule, MatInputModule, MatRadioModule,
+  MatListModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
