98d274447034f5dcb56b588f09c8401494243777
susan nerdlinger <susannerdlinger@susans-MacBook-Pro.local>
2019-03-04 13:05:37 -0500

    backend and ResourcePageComponent

---
+++ src/app/resource-page/resource-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-page',
+  templateUrl: './resource-page.component.html',
+  styleUrls: ['./resource-page.component.css']
+})
+export class ResourcePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
