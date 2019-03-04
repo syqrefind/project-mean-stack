2a5db41fee9e8b35fedd3fdb17cfa50af3be1773
syqrefind <syqrefind@gmail.com>
2019-03-04 17:06:42 -0500

    update LoginComponent

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  //   this.route.queryParams.subscribe(params => {
+  //     console.log(params);
+  //     if (params.msg === 'doomed') {
+  //       console.log('You forgot your password. You are DOOMED.');
+  //     }
+  // });
   }
 
 }
