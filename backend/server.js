d98e6127793fbb8c5bd872267c6cdff4e3bacc39
susan nerdlinger <susannerdlinger@susans-MacBook-Pro.local>
2019-03-04 12:10:40 -0500

    creat server and backend structure,connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
+
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-
-const  Post=require("./post");
-const app=express();
+const  Post=require("./models/post");
+const   apps=express();
 
 mongoose
  .connect(
-"mongodb+srv://new-admin-0:8415syq_@my-dream-cluster-seo5i.mongodb.net/test?retryWrites=true"
+"mongodb+srv://new-admin-0:8415syq_@my-dream-cluster-seo5i.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true }
  )
  .then(()=>{
      console.log("Connect to database!");
@@ -15,3 +15,15 @@ mongoose
  .catch(()=>{
     console.log("Connect failed!");
  })
+
+const http = require('http');
+const app= require('./app');
+
+const port = process.env.PORT||3000;
+
+app.set('port'.port);
+const server = http.createServer(app);
+
+server.listen(port);
+
+
