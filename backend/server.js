02c1fc5fb5b0bafa794afb998de5048a8bf1665b
syqrefind <syqrefind@gmail.com>
2019-03-05 17:05:31 -0500

    backend for Registration Page

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,54 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const mongoose=require("mongoose");
-const Post=require("./models/post");
-const apps=express();
+const app = require("./app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-mongoose
- .connect(
-"mongodb+srv://new-admin-0:8415syq_@my-dream-cluster-seo5i.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true }
- )
- .then(()=>{
-     console.log("Connect to database!");
- })
- .catch(()=>{
-    console.log("Connect failed!");
- })
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-const http = require('http');
-const app= require('./app');
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-const port = process.env.PORT||3000;
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-app.set('port'.port);
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
-
+server.on("error", onError);
+server.on("listening", onListening);
 server.listen(port);
 
 
