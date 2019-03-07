b3c133660a0f53464002b2eaba5ee85a1af9eb42
syqrefind <syqrefind@gmail.com>
2019-03-07 10:30:28 -0500

    minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, } from '@angular/router';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ResourcePageComponent } from './resource-page/resource-page.component';
 import { RegistrationComponent } from './registration/registration.component';
