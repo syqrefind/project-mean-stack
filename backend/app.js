13f1e6f8aac94f7dd3164ac2823df50e9b3bb265
syqrefind <syqrefind@gmail.com>
2019-03-06 21:18:26 -0500

    minor

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("images")));
 
-app.use((req, res, next) => {
-    console.log('received request: ' + req);
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('received request: ' + req);
+//     next();
+// });
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
