efb626cef61b6d4df5ca2fc7563343507ac37a44
Igor Eskin <eskinhd@gmail.com>
2019-03-11 13:05:42 -0400

    Add Field button adds a field

--- src/app/template-page/template-page.component.ts
+++ src/app/template-page/template-page.component.ts
@@ -7,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplatePageComponent implements OnInit {
 
+  // renderTemplate = false;
+  // counter: number = 0;
+  // counterPlus: number = 0;
+
+  list = [];
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onAddField() {
-    console.log("Adding field!")
+    console.log("Adding field!");
+    this.list.push(1);
+    console.log(this.list)
+    // this.counterPlus++;
+    // if(this.counterPlus > this.counter) {
+    //   this.renderTemplate = true;
+    //   this.counter = this.counterPlus;
+    // }
   }
 
 }
