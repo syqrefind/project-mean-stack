7458347912ef82d5bae7b8c51e699392c443872a
syqrefind <syqrefind@gmail.com>
2019-03-12 10:23:06 -0400

    Minor: debug userController

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,36 +26,47 @@ exports.createUser = (req, res, next) => {
 
 exports.userLogin = (req, res, next) => {
   let fetchedUser;
+  // console.log(req.body.email);
+  // console.log(req.body.password);
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
+        console.log('user not found');
         return res.status(401).json({
           message: "Auth failed"
           // user not found
         });
       }
       fetchedUser = user;
+      // console.log('user found');
+      // console.log(fetchedUser.email + ' ' + fetchedUser._id + ' before then');
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
       if (!result) {
+        // console.log('password not match');
         return res.status(401).json({
           message: "Auth failed"
           // no result
         });
       }
+      // console.log('password matches');
+      // console.log(fetchedUser.email + ' ' + fetchedUser._id);
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id },
         "secret_this_should_be_longer",
         { expiresIn: "1h" }
       );
+      // console.log('token created');
       res.status(200).json({
         token: token,
         expiresIn: 3600,
         userId: fetchedUser._id
       });
+      return;
     })
     .catch(err => {
+      // console.log('error catched');
       return res.status(401).json({
         message: "Invalid authentication credentials!"
         // problem occurs when generating or sending jwt to frontend
